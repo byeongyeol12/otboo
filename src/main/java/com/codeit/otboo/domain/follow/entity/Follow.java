3a1113016771579a51e069b3b96dea626c9453cd
손동혁 <sond5510@naver.com>
2025-06-25 17:20:38 +0900

    feat : follow,notification,dm entity 추가

--- src/main/java/com/codeit/otboo/domain/follow/entity/Follow.java
+++ src/main/java/com/codeit/otboo/domain/follow/entity/Follow.java
@@ -1,5 +1,44 @@
 package com.codeit.otboo.domain.follow.entity;
 
-public class Follow {
+import java.util.UUID;
 
+import com.codeit.otboo.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "follows")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private UUID id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "followerId", nullable = false)
+	private User follower;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "followingId", nullable = false)
+	private User following;
+
+	@Builder
+	public Follow(User follower, User following) {
+		this.follower = follower;
+		this.following = following;
+	}
 }
