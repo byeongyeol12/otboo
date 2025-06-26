adc1744e728c45265df7992b1aac94a7bcdc0f1d
손동혁 <sond5510@naver.com>
2025-06-26 15:07:13 +0900

    refactor : FollowRepository for Agile

--- src/main/java/com/codeit/otboo/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/codeit/otboo/domain/follow/repository/FollowRepository.java
@@ -6,10 +6,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codeit.otboo.domain.follow.entity.Follow;
-import com.codeit.otboo.domain.follow.entity.User;
 
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
-	boolean existsByFollowerAndFollowing(User follower,User following); // 중복 팔로우 방지 체크
+	boolean existsByFollowerIdAndFollowingId(UUID followerId, UUID followingId); // 중복 팔로우 방지 체크
 	List<Follow> findAllByFollowerId(UUID followerId); // 내가 팔로우하는 사람들
 	List<Follow> findAllByFollowingId(UUID followeeId); //나를 팔로우하는 사람들
 }
