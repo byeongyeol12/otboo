872e31b490558a8d7ca136643c3d848db25ff38b
손동혁 <sond5510@naver.com>
2025-06-30 17:32:32 +0900

    feat : 알림 목록 조회 controller

--- src/main/java/com/codeit/otboo/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/otboo/domain/notification/controller/NotificationController.java
@@ -1,5 +1,37 @@
 package com.codeit.otboo.domain.notification.controller;
 
-public class NotificationController {
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codeit.otboo.domain.notification.dto.NotificationDtoCursorResponse;
+import com.codeit.otboo.domain.notification.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notifications")
+public class NotificationController {
+	private final NotificationService notificationService;
+
+	//알림 목록 조회
+	@GetMapping
+	public ResponseEntity<NotificationDtoCursorResponse> getNotifications(
+		@RequestParam(required = false) String cursor,
+		@RequestParam(required = false) String idAfter,
+		int limit,
+		@RequestParam(required = false) UUID userId // 추후 인증 토큰에서 추출
+
+	) {
+		NotificationDtoCursorResponse notificationDtoCursorResponse = notificationService.getNotifications(userId,cursor,idAfter,limit);
+		return ResponseEntity.status(HttpStatus.OK).body(notificationDtoCursorResponse);
+	}
+
+	//알림 읽음 처리
 }
