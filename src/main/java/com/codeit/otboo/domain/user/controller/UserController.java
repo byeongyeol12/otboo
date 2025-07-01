e260d6180362129761ab3e2b9bc173a2dc1ccc09
Kim EungJin <mmm806@naver.com>
2025-07-01 18:40:41 +0900

    :sparkles:  UserController 회원가입, 권한 변경, 유저 목록 조회 API 구현 #9

--- src/main/java/com/codeit/otboo/domain/user/controller/UserController.java
+++ src/main/java/com/codeit/otboo/domain/user/controller/UserController.java
@@ -1,5 +1,58 @@
 package com.codeit.otboo.domain.user.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codeit.otboo.domain.user.dto.request.UserCreateRequest;
+import com.codeit.otboo.domain.user.dto.request.UserRoleUpdateRequest;
+import com.codeit.otboo.domain.user.dto.request.UserSearchRequest;
+import com.codeit.otboo.domain.user.dto.response.UserDto;
+import com.codeit.otboo.domain.user.dto.response.UserDtoCursorResponse;
+import com.codeit.otboo.domain.user.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+	private final UserService userService;
+
+	@PostMapping
+	public ResponseEntity<UserDto> singup(@Valid @RequestBody UserCreateRequest request) {
+		UserDto userDto = userService.create(request);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
+	}
+
+	@GetMapping
+	//@PreAuthorize("hasRole(ADMIN)")
+	public ResponseEntity<UserDtoCursorResponse> getAllUsers(
+		@Valid @ModelAttribute UserSearchRequest request
+	) {
+		UserDtoCursorResponse response = userService.searchUsers(request);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@PatchMapping("/{userId}/role")
+	//@PreAuthorize("hasRole(ADMIN)")
+	public ResponseEntity<UserDto> updateUserRole(
+		@PathVariable UUID userId,
+		@RequestBody @Valid UserRoleUpdateRequest request
+	) {
+		UserDto updateUser = userService.updateUserRole(userId, request);
+		return ResponseEntity.ok(updateUser);
+	}
 }
