d5139a3dbf5d3dfb2f9155c0bc76f24038d66c12
Kim EungJin <mmm806@naver.com>
2025-07-01 18:46:15 +0900

    :sparkles:  [#9] Spring Security 및 QueryDSL 설정 추가

---
+++ src/main/java/com/codeit/otboo/global/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.codeit.otboo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.codeit.otboo.global.config.jwt.JwtAuthenticationFilter;
+import com.codeit.otboo.global.config.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // ✅ 세션 X
+			.headers(headers -> headers
+				.frameOptions(frame -> frame.sameOrigin())
+			)
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					"/api/users",            // 회원가입
+					"/api/auth/**",
+					"/h2-console/**"
+				).permitAll()
+				.anyRequest().authenticated()
+			)
+			.addFilterBefore( // ✅ JWT 필터 등록
+				new JwtAuthenticationFilter(jwtTokenProvider),
+				UsernamePasswordAuthenticationFilter.class
+			)
+			.httpBasic(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
