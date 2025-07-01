c2b51707b2279f9dca45f40c41cffc1c536160b2
손동혁 <sond5510@naver.com>
2025-07-01 18:29:14 +0900

    feat : NotificationMapper

---
+++ src/main/java/com/codeit/otboo/domain/notification/mapper/NotificationMapper.java
@@ -0,0 +1,16 @@
+package com.codeit.otboo.domain.notification.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.codeit.otboo.domain.notification.dto.NotificationDto;
+import com.codeit.otboo.domain.notification.entity.Notification;
+
+@Mapper(componentModel = "spring")
+public interface NotificationMapper {
+	NotificationMapper INSTANCE = Mappers.getMapper(NotificationMapper.class);
+
+	@Mapping(target = "receiverId", source = "receiver.id")
+	NotificationDto toNotificationDto(Notification notification);
+}
