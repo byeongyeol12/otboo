8bbe6228287288ade89871078e97d93b35d56f69
Kim EungJin <mmm806@naver.com>
2025-07-02 16:16:14 +0900

    :lock:  [#9] Cookie 기반 CSRF 방어 설정 추가

--- src/main/java/com/codeit/otboo/global/config/SecurityConfig.java
+++ src/main/java/com/codeit/otboo/global/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import com.codeit.otboo.global.config.jwt.JwtAuthenticationFilter;
 import com.codeit.otboo.global.config.jwt.JwtTokenProvider;
@@ -24,7 +25,9 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
+			.csrf(csrf -> csrf
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			)
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // ✅ 세션 X
 			.headers(headers -> headers
