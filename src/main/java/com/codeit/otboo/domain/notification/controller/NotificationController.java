68081dd094c14c5857e20f76e5c155eae4920dbe
손동혁 <sond5510@naver.com>
2025-07-02 13:42:47 +0900

    feat : Add readNotifications(알림 일읽음처리) to NotificationController

--- src/main/java/com/codeit/otboo/domain/notification/controller/NotificationController.java
+++ src/main/java/com/codeit/otboo/domain/notification/controller/NotificationController.java
@@ -4,7 +4,10 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +37,12 @@ public class NotificationController {
 	}
 
 	//알림 읽음 처리
+	@DeleteMapping("/{notificationId}")
+	public ResponseEntity<Void> readNotifications(
+		@AuthenticationPrincipal OtbooUserDetails userDetails,
+		@PathVariable UUID notificationId
+	) {
+		notificationService.readNotifications(notificationId,userDetails.getUserDto().id());
+		return ResponseEntity.noContent().build();
+	}
 }
