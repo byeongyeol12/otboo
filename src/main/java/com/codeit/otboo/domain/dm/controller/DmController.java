fb3ad2da7954b6426b4931e5d6303199885e8b04
손동혁 <sond5510@naver.com>
2025-07-03 17:45:06 +0900

    feat : Add getDms to DmController

--- src/main/java/com/codeit/otboo/domain/dm/controller/DmController.java
+++ src/main/java/com/codeit/otboo/domain/dm/controller/DmController.java
@@ -1,5 +1,37 @@
 package com.codeit.otboo.domain.dm.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codeit.otboo.domain.dm.dto.DirectMessageDto;
+import com.codeit.otboo.domain.dm.dto.DirectMessageDtoCursorResponse;
+import com.codeit.otboo.domain.dm.service.DmService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class DmController {
+	private final DmService dmService;
+
+	//메시지 목록 조회
+	@GetMapping("/api/direct-messages")
+	public ResponseEntity<DirectMessageDtoCursorResponse> getDms(
+		@RequestParam UUID userId,
+		@RequestParam String cursor,
+		@RequestParam UUID idAfter,
+		@RequestParam int limit
+	){
+		return ResponseEntity.status(HttpStatus.OK).body();
+	}
 
 }
