55084aaedea93b8cd06ec91f7c4a0a1cd57861a7
손동혁 <sond5510@naver.com>
2025-07-07 14:44:45 +0900

    refactor : UserSummaryDto

--- src/main/java/com/codeit/otboo/domain/user/dto/response/UserSummaryDto.java
+++ src/main/java/com/codeit/otboo/domain/user/dto/response/UserSummaryDto.java
@@ -2,10 +2,19 @@ package com.codeit.otboo.domain.user.dto.response;
 
 import java.util.UUID;
 
+import com.codeit.otboo.domain.user.entity.User;
+
 public record UserSummaryDto(
 	UUID id,
 	String name,
 	String profileImageUrl
 ) {
 
+	public static UserSummaryDto from(User user) {
+		return new UserSummaryDto(
+			user.getId(),
+			user.getName(),
+			user.getProfile().getProfileImageUrl()
+		);
+	}
 }
