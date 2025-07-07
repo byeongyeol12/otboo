87a3a5e744fa4b551179bc3d3ce2d5397ddf3fde
손동혁 <sond5510@naver.com>
2025-07-07 14:07:52 +0900

    refactor : AuthenticationPrincipal to FollowController

--- src/main/java/com/codeit/otboo/domain/follow/controller/FollowController.java
+++ src/main/java/com/codeit/otboo/domain/follow/controller/FollowController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.codeit.otboo.domain.follow.dto.FollowDto;
 import com.codeit.otboo.domain.follow.dto.FollowListResponse;
 import com.codeit.otboo.domain.follow.dto.FollowSummaryDto;
 import com.codeit.otboo.domain.follow.service.FollowService;
+import com.codeit.otboo.global.config.security.UserPrincipal;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,28 +30,32 @@ import lombok.RequiredArgsConstructor;
 public class FollowController {
 	private final FollowService followService;
 
-	//팔로우 생성
+	//팔로우 생성(
 	@PostMapping
-	public ResponseEntity<FollowDto> createFollow(@Valid @RequestBody FollowCreateRequest request) {
-		FollowDto followDto = followService.createFollow(request);
+	public ResponseEntity<FollowDto> createFollow(
+		@Valid @RequestBody FollowCreateRequest request, // 유저가 팔로우한 사람(팔로이)
+		@AuthenticationPrincipal UserPrincipal userPrincipal // 로그인한 유저(팔로워)
+	) {
+		UUID myUserId = userPrincipal.getId();
+		FollowDto followDto = followService.createFollow(myUserId,request.followeeId());
 		return ResponseEntity.status(HttpStatus.CREATED).body(followDto);
 	}
 
 	//팔로우 요약 정보 조회
 	@GetMapping("/summary")
 	public ResponseEntity<FollowSummaryDto> getFollowsSummary(
-		@RequestParam UUID userId, // 대상 유저 id
-		@RequestParam UUID myUserId
-		//@AuthenticationPrincipal CustomUserDetails currentUser //JWT 기반 인증
+		@RequestParam UUID userId, // 팔로우 요약 정보를 가져오는 대상
+		@AuthenticationPrincipal UserPrincipal userPrincipal // 로그인한 유저
 	) {
+		UUID myUserId = userPrincipal.getId();
 		FollowSummaryDto followSummaryDtoList = followService.getFollowSummary(userId,myUserId);
 		return ResponseEntity.status(HttpStatus.OK).body(followSummaryDtoList);
 	}
 
-	//팔로잉 목록 조회(내가 팔로우한 사람들)
+	//팔로잉 목록 조회(유저가 팔로우한 사람들)
 	@GetMapping("/followings")
 	public ResponseEntity<FollowListResponse> getFollowings(
-		@RequestParam UUID followerId,
+		@AuthenticationPrincipal UserPrincipal userPrincipal, // 로그인한 유저
 		@RequestParam(required = false) String cursor,
 		@RequestParam(required = false) UUID idAfter,
 		@RequestParam int limit,
@@ -57,13 +63,15 @@ public class FollowController {
 		@RequestParam(required = false) String sortBy,
 		@RequestParam(required = false) String sortDirection
 	) {
-		FollowListResponse followingList = followService.getFollowings(followerId,cursor,idAfter,limit,nameLike,sortBy,sortDirection);
+		UUID myUserId = userPrincipal.getId();
+		FollowListResponse followingList = followService.getFollowings(myUserId,cursor,idAfter,limit,nameLike,sortBy,sortDirection);
 		return ResponseEntity.status(HttpStatus.OK).body(followingList);
 	}
-	//팔로워 목록 조회
+
+	//팔로워 목록 조회(유저를 팔로우한 사람들)
 	@GetMapping("/followers")
 	public ResponseEntity<FollowListResponse> getFollowers(
-		@RequestParam UUID followeeId,
+		@AuthenticationPrincipal UserPrincipal userPrincipal, // 로그인한 유저
 		@RequestParam(required = false) String cursor,
 		@RequestParam(required = false) UUID idAfter,
 		@RequestParam int limit,
@@ -71,14 +79,19 @@ public class FollowController {
 		@RequestParam(required = false) String sortBy,
 		@RequestParam(required = false) String sortDirection
 	) {
-		FollowListResponse followerList = followService.getFollowers(followeeId,cursor,idAfter,limit,nameLike,sortBy,sortDirection);
+		UUID myUserId = userPrincipal.getId();
+		FollowListResponse followerList = followService.getFollowers(myUserId,cursor,idAfter,limit,nameLike,sortBy,sortDirection);
 		return ResponseEntity.status(HttpStatus.OK).body(followerList);
 	}
 
 	//팔로우 취소
 	@DeleteMapping("/{followId}")
-	public ResponseEntity<Void> cancelFollow(@PathVariable UUID followId,@RequestParam UUID loginUserId) { // 추후 JWT 에서 추출
-		followService.cancelFollow(followId, loginUserId);
+	public ResponseEntity<Void> cancelFollow(
+		@PathVariable UUID followId,
+		@AuthenticationPrincipal UserPrincipal userPrincipal  // 로그인한 유저
+	) {
+		UUID myUserId = userPrincipal.getId();
+		followService.cancelFollow(followId, myUserId);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
