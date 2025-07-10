a80f5d341705b25f743617cfcf339055850975f2
ghtkdrla <ghtkdrla321@naver.com>
2025-07-11 07:58:56 +0900

    :sparkles: baseEntity 추가

--- src/main/java/com/codeit/otboo/global/base/BaseUpdatableEntity.java
+++ src/main/java/com/codeit/otboo/global/base/BaseUpdatableEntity.java
@@ -1,2 +1,20 @@
-package com.codeit.otboo.global.base;public class BaseUpdatableEntity {
+package com.codeit.otboo.global.base;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@Getter
+public class BaseUpdatableEntity extends BaseEntity {
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private Instant updatedAt;
 }
