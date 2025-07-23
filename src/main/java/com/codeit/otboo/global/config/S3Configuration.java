daed735a9f991aad3017e75f7e785d38bbd55d26
뺑열 <byeongyeol12@gmail.com>
2025-07-23 11:45:45 +0900

    S3Configuration 수정

--- src/main/java/com/codeit/otboo/global/config/S3Configuration.java
+++ src/main/java/com/codeit/otboo/global/config/S3Configuration.java
@@ -4,31 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Configuration {
-	// application.yml (또는 환경 변수)에서 AWS 자격 증명 주입
-	@Value("${cloud.aws.credentials.access-key}")
-	private String awsAccessKey;
 
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String awsSecretKey;
-
-	// application.yml (또는 환경 변수)에서 AWS 리전 주입
 	@Value("${cloud.aws.region.static}")
-	private String awsRegion;
+	private String region;
+
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucketName;
 
 	@Bean
 	public S3Client s3Client() {
-		AwsBasicCredentials credentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
-
 		return S3Client.builder()
-			.region(Region.AP_NORTHEAST_2)
-			.credentialsProvider(StaticCredentialsProvider.create(credentials))
+			.region(Region.of(this.region))
 			.build();
 	}
 }
