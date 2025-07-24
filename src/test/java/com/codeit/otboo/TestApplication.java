ace2d0bd980607863a25e1c35888cd7272de9f7a
Kim EungJin <mmm806@naver.com>
2025-07-24 17:46:54 +0900

    :truck: [#64] TestApplication 위치 수정

--- src/test/java/com/codeit/otboo/TestApplication.java
+++ src/test/java/com/codeit/otboo/TestApplication.java
@@ -1,12 +1,15 @@
-package com.codeit.otboo.domain;
+package com.codeit.otboo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  * OtbooApplication 이 항상 테스트 컨텍스트에 포함돼
  * 컨트롤러 단위테스트에서 entityManagerFactory 를 찾다 에러 발생
  */
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.codeit.otboo"})
+@EntityScan(basePackages = {"com.codeit.otboo"})
 public class TestApplication {
 
 }
