ec268f2ce8d4c7fb1817c6028fc4f161f25bd2e9
손동혁 <sond5510@naver.com>
2025-07-24 10:57:50 +0900

    refactor : build.gradle merge error

--- src/test/java/com/codeit/otboo/domain/sse/repository/SseMessageRepositoryTest.java
+++ src/test/java/com/codeit/otboo/domain/sse/repository/SseMessageRepositoryTest.java
@@ -1,34 +1,34 @@
-// package com.codeit.otboo.domain.sse.repository;
-//
-// import java.lang.reflect.Field;
-// import java.util.UUID;
-//
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-//
-// import com.codeit.otboo.domain.sse.util.SseMessage;
-//
-// public class SseMessageRepositoryTest {
-// 	private SseMessageRepository sseMessageRepository;
-//
-// 	private int defaultCapacity = 3;
-//
-// 	@BeforeEach
-// 	void setUp() throws IllegalAccessException, NoSuchFieldException {
-// 		sseMessageRepository = new SseMessageRepository();
-// 		Field field = SseMessageRepository.class.getDeclaredField("eventQueueCapacity");
-// 		field.setAccessible(true);
-// 		field.set(sseMessageRepository,defaultCapacity);
-// 	}
-//
-// 	@Test
-// 	@DisplayName("save - 메시지 저장 성공")
-// 	void save_success() {
-// 		//given
-// 		UUID eventId = UUID.randomUUID();
-// 		UUID receiverId = UUID.randomUUID();
-// 		SseMessage sseMessage = SseMessage.create(receiverId,"test","test data");
-//
-// 	}
-// }
+package com.codeit.otboo.domain.sse.repository;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.codeit.otboo.domain.sse.util.SseMessage;
+
+public class SseMessageRepositoryTest {
+	private SseMessageRepository sseMessageRepository;
+
+	private int defaultCapacity = 3;
+
+	@BeforeEach
+	void setUp() throws IllegalAccessException, NoSuchFieldException {
+		sseMessageRepository = new SseMessageRepository();
+		Field field = SseMessageRepository.class.getDeclaredField("eventQueueCapacity");
+		field.setAccessible(true);
+		field.set(sseMessageRepository,defaultCapacity);
+	}
+
+	@Test
+	@DisplayName("save - 메시지 저장 성공")
+	void save_success() {
+		//given
+		UUID eventId = UUID.randomUUID();
+		UUID receiverId = UUID.randomUUID();
+		SseMessage sseMessage = SseMessage.create(receiverId,"test","test data");
+
+	}
+}
