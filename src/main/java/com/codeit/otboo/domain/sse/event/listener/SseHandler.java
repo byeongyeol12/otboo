b0c25d57af2e5a413acf66e2b276b61cef11a6b2
손동혁 <sond5510@naver.com>
2025-07-24 14:13:53 +0900

    refactor : sse package 구조

--- src/main/java/com/codeit/otboo/domain/sse/event/listener/SseHandler.java
+++ src/main/java/com/codeit/otboo/domain/sse/event/listener/SseHandler.java
@@ -1,4 +1,4 @@
-package com.codeit.otboo.domain.sse.util;
+package com.codeit.otboo.domain.sse.event.listener;
 
 import java.util.UUID;
 
@@ -8,6 +8,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.codeit.otboo.domain.notification.dto.NotificationDto;
 import com.codeit.otboo.domain.sse.service.SseEmitterService;
+import com.codeit.otboo.domain.sse.util.SseMessage;
 import com.codeit.otboo.exception.CustomException;
 import com.codeit.otboo.global.error.ErrorCode;
 
