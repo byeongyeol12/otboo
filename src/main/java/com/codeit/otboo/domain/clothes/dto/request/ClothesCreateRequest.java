d360ee463ca1130efa796194db2441c343833463
impmonzz <khss8070@gmail.com>
2025-07-25 13:31:08 +0900

    docs(clothes): 의상 관련 API Swagger 문서 어노테이션 추가

--- src/main/java/com/codeit/otboo/domain/clothes/dto/request/ClothesCreateRequest.java
+++ src/main/java/com/codeit/otboo/domain/clothes/dto/request/ClothesCreateRequest.java
@@ -1,24 +1,29 @@
 package com.codeit.otboo.domain.clothes.dto.request;
 
+import com.codeit.otboo.domain.clothes.dto.response.ClothesAttributeDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
-import com.codeit.otboo.domain.clothes.dto.response.ClothesAttributeDto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
+@Schema(description = "의류 등록 요청 DTO")
 public record ClothesCreateRequest(
-	@NotNull(message = "사용자 ID는 필수입니다.")
-	UUID ownerId,
 
-	@NotBlank(message = "옷 이름은 필수입니다.")
-	String name,
+		@Schema(description = "소유자(사용자) ID", example = "a8efc13e-9a11-41fa-bf68-994de9e29c7a")
+		@NotNull(message = "사용자 ID는 필수입니다.")
+		UUID ownerId,
 
-	@NotBlank(message = "의상 타입은 필수입니다.")
-	String type,
+		@Schema(description = "옷 이름", example = "블루 진")
+		@NotBlank(message = "옷 이름은 필수입니다.")
+		String name,
 
-	@NotNull(message = "속성 목록은 필수입니다.")
-	List<ClothesAttributeDto> attributes
-) {
-}
+		@Schema(description = "의상 타입 (예: TOP, BOTTOM, OUTER 등)", example = "TOP")
+		@NotBlank(message = "의상 타입은 필수입니다.")
+		String type,
+
+		@Schema(description = "속성 목록", example = "[{\"definitionId\": \"4e444ac0-5ec5-41b4-bacf-9c6d2dc0b893\", \"value\": \"화이트\"}]")
+		@NotNull(message = "속성 목록은 필수입니다.")
+		List<ClothesAttributeDto> attributes
+
+) { }
--- src/main/java/com/codeit/otboo/domain/clothes/dto/response/ClothesDto.java
+++ src/main/java/com/codeit/otboo/domain/clothes/dto/response/ClothesDto.java
@@ -1,14 +1,28 @@
 package com.codeit.otboo.domain.clothes.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.UUID;
 
+@Schema(description = "의류 정보 DTO")
 public record ClothesDto(
-	UUID id,
-	UUID ownerId,
-	String name,
-	String imageUrl,
-	String type,
-	List<ClothesAttributeWithDefDto> attributes
-) {
-}
+
+		@Schema(description = "의류 ID", example = "f0216d30-f135-4ad3-b1e0-4100a49b553d")
+		UUID id,
+
+		@Schema(description = "소유자(사용자) ID", example = "a8efc13e-9a11-41fa-bf68-994de9e29c7a")
+		UUID ownerId,
+
+		@Schema(description = "의류 이름", example = "블루 진")
+		String name,
+
+		@Schema(description = "의류 이미지 URL", example = "https://cdn.example.com/clothes/123.jpg")
+		String imageUrl,
+
+		@Schema(description = "의류 타입 (예: TOP, BOTTOM, OUTER 등)", example = "TOP")
+		String type,
+
+		@Schema(description = "의류 속성 리스트")
+		List<ClothesAttributeWithDefDto> attributes
+
+) { }
