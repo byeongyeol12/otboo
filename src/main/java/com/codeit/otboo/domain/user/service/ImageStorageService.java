c10737b68f33533ced1121aa4bf4150b19746695
Kim EungJin <mmm806@naver.com>
2025-07-25 10:16:01 +0900

    :recycle: [#64] 프로필 이미지 S3 업로드로 수정

--- src/main/java/com/codeit/otboo/domain/user/service/ImageStorageService.java
+++ src/main/java/com/codeit/otboo/domain/user/service/ImageStorageService.java
@@ -3,5 +3,5 @@ package com.codeit.otboo.domain.user.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageStorageService {
-	String upload(MultipartFile file);
+	String upload(MultipartFile file, String folder);
 }
