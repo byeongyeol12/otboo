3db9d0d4effa07b4d3b378ca5b7acc8fe2a052c3
ghtkdrla <ghtkdrla321@naver.com>
2025-07-30 15:24:17 +0900

    일단 최신화코드

--- src/main/java/com/codeit/otboo/OtbooApplication.java
+++ src/main/java/com/codeit/otboo/OtbooApplication.java
@@ -7,16 +7,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "com.codeit.otboo")
 @EnableScheduling
 public class OtbooApplication {
 
 	public static void main(String[] args) {
-		// .env 파일을 로드하고 시스템 속성으로 설정합니다.
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		// bring profile
+		String activeProfile =
+			System.getProperty("spring.profiles.active",
+				System.getenv().getOrDefault("SPRING_PROFILES_ACTIVE", "dev"));
+
+		// apply dotenv only in case of dev profile
+		if ("dev".equals(activeProfile)) {
+			Dotenv.configure()
+				.ignoreIfMissing()
+				.load()
+				.entries()
+				.forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+				);
+		}
 
 		SpringApplication.run(OtbooApplication.class, args);
 	}
-
 }
